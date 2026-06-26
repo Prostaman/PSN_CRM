0eedea5c65640e951f414a3dcca6f57f686c120b
Rostyslav Trio <rvt10000@gmail.com>
2026-06-26 22:36:02 +0200

    Initial commit

---
+++ WebView.swift
@@ -0,0 +1,28 @@
+import SwiftUI
+import WebKit
+
+struct WebView: UIViewRepresentable {
+    let url: URL
+
+    func makeUIView(context: Context) -> WKWebView {
+        let config = WKWebViewConfiguration()
+        let webView = WKWebView(frame: .zero, configuration: config)
+        webView.allowsBackForwardNavigationGestures = true
+        webView.scrollView.contentInsetAdjustmentBehavior = .never
+        // Start loading immediately
+        let request = URLRequest(url: url)
+        webView.load(request)
+        return webView
+    }
+
+    func updateUIView(_ uiView: WKWebView, context: Context) {
+        guard uiView.url != url else { return }
+        uiView.load(URLRequest(url: url))
+    }
+}
+
+#if DEBUG
+#Preview {
+    WebView(url: URL(string: "https://crm.poehalisnami.ua")!)
+}
+#endif
